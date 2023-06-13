a1a71afbc1963d06a583dfb704c35f65a77fe02c
u-know-what-i-mean <maxsmart1996@gmail.com>
2023-06-13 22:07:28 +0300

    Add workflow with PEP8 linting

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,6 @@
+from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework import status
 
 from .models import Task
 from .serializers import TaskSerializer
